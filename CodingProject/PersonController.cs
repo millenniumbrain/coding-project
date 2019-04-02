ab2ab190cb275f32074f6ab4589c81a082eb1836
William Wilson <wlwilson96@gmail.com>
2019-04-02 02:52:07 -0400

    Added some design improvements and validation for date of birth

--- CodingProject/PersonController.cs
+++ CodingProject/PersonController.cs
@@ -34,28 +34,44 @@ namespace CodingProject
             string intSec = "Integrated Security=SSPI;";
             string interviewConnString = "Initial Catalog=interview;" + dataSrc + intSec;
             using (var interviewConn = new SqlConnection(interviewConnString))
-            {
-                var person = new Person
+            {   try
                 {
-                    DBConnection = interviewConn,
-                    FirstName = formData["firstName"],
-                    LastName = formData["lastName"],
-                    DOB = formData["dob"]
-                };
-                Person savedPerson = person.Save();
-                var address = new Address
+                    var person = new Person
+                    {
+                        DBConnection = interviewConn,
+                        FirstName = formData["firstName"],
+                        LastName = formData["lastName"],
+                        DOB = formData["dob"]
+                    };
+                    Person savedPerson = person.Save();
+                    var address = new Address
+                    {
+                        DBConnection = interviewConn,
+                        StreetOne = formData["streetOne"].ToString(),
+                        StreetTwo = formData["streetTwo"].ToString(),
+                        City = formData["city"].ToString(),
+                        State = formData["state"].ToString(),
+                        ZipCode = formData["zipCode"].ToString(),
+                        PersonID = savedPerson.Id
+                    };
+                    savedPerson.Addresses.Add(address.Save());
+                    var jsonMsg = new Dictionary<string, string>
+                    {
+                        {"msg","Personal Info was added to the database"}
+                    };
+                    string json = JsonConvert.SerializeObject(jsonMsg);
+                    return Content(json, "application/json");
+                }
+                catch (Exception error)
                 {
-                    DBConnection = interviewConn,
-                    StreetOne = formData["streetOne"].ToString(),
-                    StreetTwo = formData["streetTwo"].ToString(),
-                    City = formData["city"].ToString(),
-                    State = formData["state"].ToString(),
-                    ZipCode = formData["zipCode"].ToString(),
-                    PersonID = savedPerson.Id
-                };
-                savedPerson.Addresses.Add(address.Save());
-                string json = JsonConvert.SerializeObject(savedPerson);
-                return Content(json, "application/json");
+                    System.Diagnostics.Debug.WriteLine(error);
+                    var jsonMsg = new Dictionary<string, string>
+                    {
+                        {"msg","Error adding info to database. Please try again."}
+                    };
+                    string json = JsonConvert.SerializeObject(jsonMsg);
+                    return Content(json, "application/json");
+                }
             }
         }
     }
--- CodingProject/wwwroot/js/main.js
+++ CodingProject/wwwroot/js/main.js
@@ -4,45 +4,68 @@ var $retrieve = $("#retrieveButton");
 $form.submit(function (event) {
     event.preventDefault();
     var formData = new FormData(document.getElementById("form"));
-    console.log(formData);
-    $.ajax({
-        url: "/person",
-        data: formData,
-        type: "POST",
-        processData: false,
-        contentType: false
-    })
+
+    // date validation
+    var dateFormat = /^\d{2}\/\d{2}\/\d{4}$/;
+    if (!dateFormat.test($("#dob").val())) {
+        $("#dob").get(0).setCustomValidity("Wrong date format! Use MM/MM/YYYY, ex: 01/01/1969");
+        return false;
+    } else {
+        $("#dob").get(0).setCustomValidity("");
+        // end function and prevent ajax trigger
+    }
+
+    if ($form.get(0).checkValidity()) {
+        $.ajax({
+            url: "/person",
+            data: formData,
+            type: "POST",
+            processData: false,
+            contentType: false
+        }).done(function (data) {
+            var $formMsg = $("#formMsg");
+            $formMsg.text(data["msg"]);
+            $formMsg.show();
+            setTimeout(function () {
+                $formMsg.hide();
+            }, 5000);
+        });
+    } 
+
 });
 
 $retrieve.click(function () {
     $.get("/person", function (data) {
         var table = document.getElementById("tableOwn");
-        table.deleteRow(0);
-        for (var i = 0; i < data.length; i++) {
-            var row = document.createElement("tr");
-            var firstName = document.createElement("td");
-            firstName.textContent = data[i]["FirstName"];
-            var lastName = document.createElement("td");
-            lastName.textContent = data[i]["LastName"];
-            var dob = document.createElement("td");
-            dob.textContent = data[i]["DOB"];
-            var address = document.createElement("td");
-            address.textContent = data[i]["Addresses"][0]["StreetOne"] + " " + data[i]["Addresses"][0]["StreetTwo"];
-            var zipCode = document.createElement("td");
-            zipCode.textContent = data[i]["Addresses"][0]["ZipCode"];
-            var city = document.createElement("td");
-            city.textContent = data[i]["Addresses"][0]["City"];
-            var state = document.createElement("td");
-            state.textContent = data[i]["Addresses"][0]["State"];
-            row.appendChild(firstName);
-            row.appendChild(lastName);
-            row.appendChild(address);
-            row.appendChild(dob);
-            row.appendChild(zipCode);
-            row.appendChild(city);
-            row.appendChild(state);
 
-            table.appendChild(row);
+        if (table.rows.length < data.length) {
+            table.deleteRow(0);
+            for (var i = 0; i < data.length; i++) {
+                var row = document.createElement("tr");
+                var firstName = document.createElement("td");
+                firstName.textContent = data[i]["FirstName"];
+                var lastName = document.createElement("td");
+                lastName.textContent = data[i]["LastName"];
+                var dob = document.createElement("td");
+                dob.textContent = data[i]["DOB"];
+                var address = document.createElement("td");
+                address.textContent = data[i]["Addresses"][0]["StreetOne"] + " " + data[i]["Addresses"][0]["StreetTwo"];
+                var zipCode = document.createElement("td");
+                zipCode.textContent = data[i]["Addresses"][0]["ZipCode"];
+                var city = document.createElement("td");
+                city.textContent = data[i]["Addresses"][0]["City"];
+                var state = document.createElement("td");
+                state.textContent = data[i]["Addresses"][0]["State"];
+                row.appendChild(firstName);
+                row.appendChild(lastName);
+                row.appendChild(address);
+                row.appendChild(dob);
+                row.appendChild(city);
+                row.appendChild(state);
+                row.appendChild(zipCode);
+                console.log("Moosse")
+                table.appendChild(row);
+            }
         }
     });
-})
+});
